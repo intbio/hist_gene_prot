e859743c6ed46bdaf7d787af66e6de6b546b58bf
l-singh-biomsu <52165734+l-singh-biomsu@users.noreply.github.com>
2020-03-11 19:10:51 +0300

    Update disp_func.js

--- docs/disp_func.js
+++ docs/disp_func.js
@@ -168,7 +168,7 @@
         if (pmids) {
             var pmids_list = pmids.split(" ");
             var links_str = "<a href='https://www.ncbi.nlm.nih.gov/pubmed/" + pmids_list[0] + "'>" + pmids_list[0] + "</a>";
-            if (pmids_list[0].search("^[0-9]+$" < 0)
+            if (pmids_list[0].search("^[0-9]+$" < 0))
                 links_str = "" + pmids_list[0];
             for (i = 1; i < pmids_list.length; i++) {
                 if (pmids_list[i].search("^[0-9]+$" < 0)
